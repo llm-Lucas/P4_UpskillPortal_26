40be6aceb9e5e4e7232b5f9fb12f18dfa35da9a4
llm-Lucas <lucaslemosm@hotmail.com>
2026-06-02 11:02:41 +0100

    Fazer funcionar a visualização do ficheiro subemtido pelo aluno

--- PortalUpskill.App/Program.cs
+++ PortalUpskill.App/Program.cs
@@ -67,6 +67,8 @@ builder.Services.AddScoped<IAnoLetivoData, AnoLetivoData>();
 builder.Services.AddScoped<EmailService>();
 builder.Services.AddScoped<ICursoCoordenadorData, CursoCoordenadorData>();
 
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
 // Register Syncfusion license
